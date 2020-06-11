837f00ce230088c03aac85a33e9c7a39e5e9c7a0
Moritz Rosar <moritzrosar@Moritzs-MacBook-Pro.local>
2020-06-11 19:12:00 +0200

    first commit

---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "eu-central-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-central-1 = "ami-337be65c"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "centos"
+}
